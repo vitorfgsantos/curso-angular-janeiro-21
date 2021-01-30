9ca0835bd9127858de6f18e7067852ec8b18580d
Vitor Farias <vitorfgsantos@outlook.com>
2021-01-30 17:35:19 -0300

    utilizando o arquivo enviroment.ts

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 import { AuthService } from '../shared/services/auth/auth.service';
 import { LoginCredenciais, LoginResponse } from './login.interfaces';
@@ -11,13 +12,15 @@ import { LoginCredenciais, LoginResponse } from './login.interfaces';
 })
 export class LoginService {
 
+  API_URL = environment.API_URL;
+
   constructor(
     private http: HttpClient,
     private authService: AuthService,
   ) { }
 
   logar(credenciais: LoginCredenciais): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>('https://accenture-java-desafio.herokuapp.com/login', credenciais)
+    return this.http.post<LoginResponse>(this.API_URL + '/login', credenciais)
       .pipe(
         tap(response => {
           this.authService.setUsuario(response.usuario);
