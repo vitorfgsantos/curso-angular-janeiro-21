fc64ae109b4fabc9936db3afaf8d8af7b615d7fc
Vitor Farias <vitorfgsantos@outlook.com>
2021-01-29 21:49:59 -0300

    lifecycle hooks do angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Seja bem-vindo!';
 
-  constructor() {
+  exibirHeader = true;
+
+  constructor() { }
+
+  ngOnInit() {
     console.log('Passou por aqui');
-    
+
     this.title = 'Outra coisa';
 
     setTimeout(() => {
       this.title = 'Seja bem-vindo!';
-    }, 3000);
+    }, 10000);
   }
 
   somar(numero1: number, numero2: number) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() titulo: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('HEADER: ngOnInit')
+  }
+
+  ngOnChanges() {
+    console.log('HEADER: ngOnChanges')
+  }
+
+  ngOnDestroy() {
+    console.log('HEADER: ngOnDestroy');
   }
 
 }
