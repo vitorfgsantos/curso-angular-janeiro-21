2ef9448ab5fb211e8991e63107f9e82c69bf1cf5
Vitor Farias <vitorfgsantos@outlook.com>
2021-01-30 14:01:12 -0300

    validacoes de formulario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  @ViewChild('usuarioInput') usuarioInput: ElementRef | undefined;
+  @ViewChild('senhaInput') senhaInput: ElementRef | undefined;
 
-  ngOnInit(): void {
+  usuario: string = '';
+  senha: string = '';
+
+  constructor(
+    private loginService: LoginService,
+  ) { }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      form.controls.usuario.markAsTouched();
+      form.controls.senha.markAsTouched();
+
+      if (form.controls.usuario.invalid) {
+        this.usuarioInput?.nativeElement.focus();
+      }
+
+      if (form.controls.senha.invalid) {
+        this.senhaInput?.nativeElement.focus();
+      }
+
+      return;
+    }
+
+    this.login();
+  }
+
+  exibeErro(nomeControle: string, form: NgForm) {
+    if (!form.controls[nomeControle]) {
+      return false;
+    }
+
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
+  }
+
+  login() {
+    
   }
 
 }
