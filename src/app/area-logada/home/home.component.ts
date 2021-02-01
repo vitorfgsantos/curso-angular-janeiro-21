cb8c5e78bff694de590d486085f890a15597430c
Vitor Farias <vitorfgsantos@outlook.com>
2021-02-01 20:41:04 -0300

    dashboard

--- src/app/area-logada/home/home.component.ts
+++ src/app/area-logada/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { finalize, take } from 'rxjs/operators';
 import { Usuario } from 'src/app/shared/interfaces/usuario.interface';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
@@ -16,6 +17,12 @@ export class HomeComponent implements OnInit {
   usuario: Usuario | undefined;
   dashboard: DashboardResponse | undefined;
 
+  inicio = '2021-01-01';
+  fim = '2021-02-01';
+
+  erro = false;
+  estaCarregando = false;
+
   constructor(
     private authService: AuthService,
     private homeService: HomeService,
@@ -27,10 +34,14 @@ export class HomeComponent implements OnInit {
   }
 
   getDashboard() {
-    const inicio = '2021-01-01';
-    const fim = '2021-02-01';
+    this.estaCarregando = true;
+    this.erro = false;
 
-    this.homeService.getDashboard(inicio, fim)
+    this.homeService.getDashboard(this.inicio, this.fim)
+      .pipe(
+        take(1),
+        finalize(() => this.estaCarregando = false)
+      )
       .subscribe(
         response => this.onSuccess(response),
         error => this.onError(error),
@@ -43,7 +54,21 @@ export class HomeComponent implements OnInit {
   }
 
   onError(error: HttpErrorResponse) {
-    console.log('Deu erro', error);
+    this.erro = true;
+  }
+
+  getBordaCardCSS(saldo: number) {
+    return {
+      'border-success': saldo > 0,
+      'border-danger': saldo < 0
+    }
+  }
+
+  getTextSaldoCSS(saldo : number) {
+    return {
+      'text-success': saldo > 0,
+      'text-danger': saldo < 0
+    }
   }
 
 }
