a58a46eeedf80d525b8b21db337629e2869a00e6
Vitor Farias <vitorfgsantos@outlook.com>
2021-02-01 21:09:41 -0300

    finalizacao do dashboard

--- src/app/area-logada/home/home.component.ts
+++ src/app/area-logada/home/home.component.ts
@@ -10,7 +10,7 @@ import { HomeService } from './home.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
